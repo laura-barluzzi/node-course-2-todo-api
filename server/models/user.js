9a93d5bc7a26fda2ace6025aadb688f34618cf3b
Laura Barluzzi <laura@koalacoder.com>
2018-01-09 15:47:50 +0000

    adds GET /users/me route and adds authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -4,39 +4,39 @@ const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1,
-        unique: true,
-        validate: {
-            validator: validator.isEmail,
-            message: '{VALUE} is not a valid email'
-        }
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+        validator: validator.isEmail,
+        message: '{VALUE} is not a valid email'
+    }
+},
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
     },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 6
-    },
-    tokens: [{
-        access: {
-            type: String,
-            required: true
-        },
-        token: {
-            type: String,
-            required: true
-        }
-    }]
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
 UserSchema.methods.toJSON = function () {
-    var user = this;
-    var userObject = user.toObject();
-    return _.pick(userObject, ['_id', 'email']);
+  var user = this;
+  var userObject = user.toObject();
+  return _.pick(userObject, ['_id', 'email']);
 };
 
 UserSchema.methods.generateAuthToken = function () {
@@ -51,6 +51,22 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+     decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+     return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
 
 var User = mongoose.model('User', UserSchema);
 
